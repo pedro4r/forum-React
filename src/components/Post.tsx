6a47330090059bed6edc6fa63fd8130f44ea9e54
Pedro Requiao <alonsofts@gmail.com>
2023-09-21 17:23:00 -0400

    add: Workflow6

---
+++ src/components/Post.tsx
@@ -0,0 +1,140 @@
+import { format, formatDistanceToNow } from "date-fns";
+import enUS from "date-fns/locale/en-US";
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from "react";
+
+import { Avatar } from "./Avatar";
+import { Comment } from "./Comment";
+import styles from "./Post.module.css";
+
+interface Author {
+  name: string;
+  role: string;
+  avatarUrl: string;
+}
+
+interface Content {
+  type: "paragraph" | "link";
+  content: string;
+}
+
+export interface PostType {
+  id: number;
+  author: Author;
+  publishedAt: Date;
+  content: Content[];
+}
+
+interface PostProps {
+  post: PostType;
+}
+
+export function Post({ post }: PostProps) {
+  const publishedDateFormatted = format(post.publishedAt, "LLLL d h:mm bbb", {
+    locale: enUS,
+  });
+  const publishedDateRelativeToNow = formatDistanceToNow(post.publishedAt, {
+    locale: enUS,
+    addSuffix: true,
+  });
+
+  //Monitoring comments
+  const [comments, setComments] = useState(["Awesome! Congratulations!! 👏👏"]);
+  //Monitoring textarea
+  const [newCommentText, setNewCommentText] = useState("");
+
+  //Post new comment function (onSubmit)
+  function handleCreateNewComment(event: FormEvent) {
+    event.preventDefault();
+    setComments([...comments, newCommentText]);
+    setNewCommentText("");
+  }
+
+  //TEXTAREA --> Apply the changes to the useState (onChange)
+  function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
+    event.target.setCustomValidity("");
+    setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
+    event.target.setCustomValidity("Esse campo é obrigatório!");
+  }
+
+  function deleteComment(commentToDelete: string) {
+    const commentsWithoutDeletedOne = comments.filter((comment) => {
+      return comment !== commentToDelete;
+    });
+
+    console.log(commentsWithoutDeletedOne);
+
+    setComments(commentsWithoutDeletedOne);
+  }
+
+  console.log(deleteComment);
+
+  const isNewCommentEmpty = newCommentText.length == 0;
+
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar hasBorder src={post.author.avatarUrl} />
+          <div className={styles.authorInfo}>
+            <strong>{post.author.name}</strong>
+            <span>{post.author.role}</span>
+          </div>
+        </div>
+
+        <time
+          title={publishedDateFormatted}
+          dateTime={post.publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        {post.content.map((line) => {
+          if (line.type == "paragraph") {
+            return <p key={line.content}>{line.content}</p>;
+          } else if (line.type == "link") {
+            return (
+              <p key={line.content}>
+                <a href="#">{line.content}</a>
+              </p>
+            );
+          }
+        })}
+      </div>
+
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
+        <strong>Leave your feedback</strong>
+        <textarea
+          name="comment"
+          placeholder="Add a comment"
+          onChange={handleNewCommentChange}
+          value={newCommentText}
+          onInvalid={handleNewCommentInvalid}
+          required
+        />
+
+        <footer>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        {comments.map((comment) => {
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          );
+        })}
+      </div>
+    </article>
+  );
+}
