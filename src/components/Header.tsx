d1d5b787bef061622efd256dc962b3d8be3f7d7f
Pedro Requiao <alonsofts@gmail.com>
2023-09-21 18:30:20 -0400

    add: Workflow9

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import igniteLogo from '../assets/ignite-logo.svg';
 export function Header() {
     return (
         <header className={styles.header}>
-            <img src ={igniteLogo} alt="logo tipo do Ignite" />
+            <img src ={igniteLogo} alt="logo Ignite" />
         </header>
     );
 }
